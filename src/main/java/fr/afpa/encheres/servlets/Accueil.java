e88c1b23282d4de73c6072edae25be8e0af07a71
Kémuel Letur <k.letur@outlook.fr>
2022-07-07 13:38:47 +0200

    Accueil.java

--- src/main/java/fr/afpa/encheres/servlets/Accueil.java
+++ src/main/java/fr/afpa/encheres/servlets/Accueil.java
@@ -2,14 +2,17 @@ package fr.afpa.encheres.servlets;
 
 
 import fr.afpa.encheres.bo.ArticlesVendus;
+import fr.afpa.encheres.bo.Categories;
 import fr.afpa.encheres.bo.Utilisateurs;
 import fr.afpa.encheres.dal.ArticlesVendusSQL;
+import fr.afpa.encheres.dal.CategoriesSQL;
 import fr.afpa.encheres.dal.UtilisateursSQL;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Date;
 import java.util.ArrayList;
 
 @WebServlet(name = "Accueil", value = "/Accueil")
@@ -22,7 +25,27 @@ public class Accueil extends HttpServlet {
             request.setAttribute("utilisateursCno_utilisateurs",utilisateursCno_utilisateurs);
         }
 
+        //Instance de classe (dal) base de données
+        ArticlesVendusSQL articlesVendusSQL = new ArticlesVendusSQL();
+        UtilisateursSQL utilisateursSQL = new UtilisateursSQL();
+        CategoriesSQL categoriesSQL = new CategoriesSQL();
 
+        //Récupération des paramètres en insert. de la base SQL
+        articlesVendusSQL.insert(new ArticlesVendus(request.getParameter("nom_article"), request.getParameter("prix_vente"), Date.valueOf(request.getParameter("date_debut_encheres")), request.getParameter("no_utilisateur")));
+
+        //Insertion des variables à partir des instances
+        ArticlesVendus articlesVendus = articlesVendusSQL.selectBylast();
+        Utilisateurs utilisateurs = utilisateursSQL.selectById(ArticlesVendus.getNo_utilisateur());
+        Utilisateurs utilisateursBis = utilisateursSQL.selectById(no_utilisateur());
+        Categories categories = categoriesSQL.selectById(Integer.parseInt(request.getParameter("no_categorie")));
+
+        //Requête d'envoie
+        request.setAttribute("articlesVendus", articlesVendus);
+        request.setAttribute("utilisateurs", utilisateurs);
+        request.setAttribute("utilisateursBis", utilisateursBis);
+        request.setAttribute("categories", categories);
+
+        //Requête d'envoie a la JSP
         request.getRequestDispatcher("WEB-INF/index.jsp").forward(request, response);
     }
 
