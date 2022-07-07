ce193e7fe7b6e67ffb76fa8f4407d1ca03a1d441
Sébastien Dijoux <sebastien.dijoux@outlook.fr>
2022-07-07 17:21:43 +0200

    Menu

--- src/main/java/fr/afpa/encheres/servlets/Modifier.java
+++ src/main/java/fr/afpa/encheres/servlets/Modifier.java
@@ -1,5 +1,8 @@
 package fr.afpa.encheres.servlets;
 
+import fr.afpa.encheres.bo.Utilisateurs;
+import fr.afpa.encheres.dal.UtilisateursSQL;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
@@ -9,7 +12,15 @@ import java.io.IOException;
 public class Modifier extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        HttpSession session = request.getSession();
+        if (session.getAttribute("no_utilisateur") != null){
+            int utilisateursCno_utilisateurs = (int) session.getAttribute("no_utilisateur");
+            request.setAttribute("utilisateursCno_utilisateurs",utilisateursCno_utilisateurs);
+        }
+        UtilisateursSQL utilisateursSQL = new UtilisateursSQL();
+        Utilisateurs utilisateurs = utilisateursSQL.selectById((int) session.getAttribute("no_utilisateur"));
+        utilisateursSQL.update((int) session.getAttribute("no_utilisateur"), utilisateurs);
+        request.getRequestDispatcher("WEB-INF/profil.jsp").forward(request,response);
     }
 
     @Override
